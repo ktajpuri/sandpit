c5164dad2fe5d3716b823af4aaf30f764b6c2e00
Kamlesh Tajouri <ktajpuri@gmail.com>
2020-06-30 09:05:15 +0530

    container with most water

---
+++ PuzzleProblems/containerWithMostWater.js
@@ -0,0 +1,50 @@
+/*
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+
+*/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+ /* Brute force
+ Execution Time:  3213514n
+
+var maxArea = function(height) {
+  let maxArea = 0;
+  for(let i=0; i<height.length -1; i++ ) {
+    for(let j=i+1; j<height.length; j++ ) {
+      maxArea=Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+    }
+  }
+  return maxArea;
+};
+ */
+
+var maxArea = function(height) {
+  let [ maxArea, i, j] = [0, 0, height.length -1];
+
+  while(i < j) {
+    maxArea = Math.max((j - i) * Math.min(height[i], height[j]), maxArea);
+    if(height[i] < height[j]) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+  return maxArea;
+};
+
+const input1 = [1,8,6,2,5,4,8,3,7];
+
+let start = process.hrtime.bigint();
+console.log(maxArea(input1));
+console.log('Execution Time: ', process.hrtime.bigint() - start);
